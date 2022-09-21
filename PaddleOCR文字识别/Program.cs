906691159c569fba3f7cfecc4a8ed72e37fb0b12
国进 颜 <guojin_yjs@cumt.edu.cn>
2022-09-21 14:45:28 +0800

    修改C#主函数

--- PaddleOCR文字识别/Program.cs
+++ PaddleOCR文字识别/Program.cs
@@ -58,7 +58,50 @@ namespace OpenVinoSharpPaddleOCR
                 result_det_byte[i] = (byte)(result_det[i] * 255);
             }
             // 重构结果图像
-            Mat image_det = new Mat(640, 640, MatType.CV_8UC3, result_det_byte);
+            Mat image_det = new Mat(640, 640, MatType.CV_8UC1, result_det_byte);
+            Cv2.ImShow("image_det", image_det);
+            // 查找文字区域狂
+            Rect[] text_rects = find_rect(image_det);
+            
+            Mat image_rect = image.Clone();
+            Cv2.Resize(image_rect, image_rect, new Size(640, 640));
+            //裁剪文字区域
+            Mat[] text_images = cut_image_roi(image_rect, text_rects);
+            // 将文字区域标注出来
+            
+            for (int r = 0; r < text_rects.Length; r++) {
+                Cv2.Rectangle(image_rect, text_rects[r], new Scalar(0, 0, 255), 2);
+            }
+            Cv2.ImShow("image_rect", image_rect);
+
+
+            //------------------------------二、文字内容识别-----------------------//
+            Console.WriteLine("//------------------------------二、文字内容识别-----------------------//");
+            //*******************1.加载模型相关信息****************//
+            // 模型相关参数
+            // 模型路径
+            string model_file_path_rec = @"D:\model\rec_server_onnx\model.onnx";
+
+            // 模型输入节点
+            string input_node_name_rec = "x";
+            // 模型输出节点
+            string output_node_name_rec = "save_infer_model/scale_0.tmp_1";
+            // 字符字典
+            string dict_path = @"E:\Git_space\基于Csharp和OpenVINO部署PaddleOCR模型\model\ppocr_keys_v1.txt";
+
+
+            //*******************2.初始化推理核心****************//
+            Core pridector_rec = new Core(model_file_path_rec, device_name);
+            // 读取字符查询字典
+            List<string> dicts = read_dict(dict_path);
+
+            //*******************3.逐张推理识别文字内容****************//
+            for (int epoch = 0; epoch < text_images.Length; epoch++) { 
+
+            }
+            
+                Console.WriteLine(12);
+            Cv2.WaitKey(0);
         }
 
 
@@ -80,13 +123,16 @@ namespace OpenVinoSharpPaddleOCR
             Rect[] rects = new Rect[contours.Length];
             for (int i = 0; i < contours.Length; i++) { 
                 Rect rect = Cv2.BoundingRect(contours[i]);
+                rect = enlarge_rect(rect);
+                Console.WriteLine(rect);
+                rects[i] = rect;
             }
             return rects;
         }
 
         public static Rect enlarge_rect(Rect rect) {
             Rect rect_temp = new Rect();
-            Point point = new Point(rect.X - rect.Width / 2, rect.Y - rect.Height / 2);
+            Point point = new Point(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
             int width = 0;
             int height = 0;
             // 判断矩形区域横纵向
@@ -100,7 +146,7 @@ namespace OpenVinoSharpPaddleOCR
                 {
                     width = (int)((double)rect.Width * 1.1);
                 }
-                height = (int)((double)rect.Height * 2.5);
+                height = (int)((double)rect.Height * 3);
             }
             else {
                 if (rect.Height < 80)
@@ -115,10 +161,51 @@ namespace OpenVinoSharpPaddleOCR
 
             }
             // 判断矩形框是否超边界
+            if (point.X - width / 2 < 0)
+            {
+                width = width + (point.X - width/2) * 2;
+            }
+            if (point.X + width / 2 > 640)
+            {
+                width = width - (640 - point.X - width/2) * 2;
+            }
+            if (point.Y - height / 2 < 0)
+            {
+                height = height + (point.Y - height/2) * 2;
+            }
+            if (point.Y + height / 2 > 640)
+            {
+                height = height - (640 - point.Y - height/2) * 2;
+            }
 
-
+            rect_temp = new Rect(point.X - width / 2 + 1, point.Y - height / 2 - 1, width - 2, height - 2);
 
             return rect_temp;
+        }
+
+        public static Mat[] cut_image_roi(Mat source_image, Rect[] rects) {
+            Mat[] rois = new Mat[rects.Length];
+
+            for (int r = 0; r < rects.Length; r++)
+            {
+                Mat roi = new Mat(source_image, rects[r]);
+                rois[r] = roi;
+            }
+            return rois;
+        
+        }
+
+        public static List<string> read_dict(string dict_path) { 
+            List<string> list = new List<string>();
+            StreamReader str = new StreamReader(dict_path);
+            while (true) { 
+                string line = str.ReadLine();
+                if (line == null) {
+                    break;
+                }
+                list.Add(line);
+            }
+            return list;
         }
     }
 }
