ca1f52e4387ec08fb90b2ada9f0c67baeff3eef4
国进 颜 <guojin_yjs@cumt.edu.cn>
2022-09-21 13:12:52 +0800

    修改

---
+++ PaddleOCR文字识别/NativeMethods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace OpenVinoSharpPaddleOCR
+{
+    internal class NativeMethods
+    {
+        private const string openvino_dll_path = @"E:\Git_space\基于Csharp和OpenVINO部署PaddleOCR模型\CppOpenVinoAPI\dll\OpenVinoSharp.dll";
+
+        [DllImport(openvino_dll_path, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr core_init(string model_file, string device_name);
+
+        [DllImport(openvino_dll_path, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr set_input_image_sharp(IntPtr inference_engine, string input_node_name, ref ulong input_size);
+
+        [DllImport(openvino_dll_path, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr set_input_data_sharp(IntPtr inference_engine, string input_node_name, ref ulong input_size);
+
+        [DllImport(openvino_dll_path, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr load_image_input_data(IntPtr inference_engine, string input_node_name, ref byte image_data, ulong image_size, int type);
+
+        [DllImport(openvino_dll_path, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr load_input_data(IntPtr inference_engine, string input_node_name, ref float input_data);
+
+        [DllImport(openvino_dll_path, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static IntPtr core_infer(IntPtr inference_engine);
+
+        [DllImport(openvino_dll_path, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void read_infer_result_F32(IntPtr inference_engine, string output_node_name, int data_size, ref float inference_result);
+
+        [DllImport(openvino_dll_path, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void read_infer_result_I32(IntPtr inference_engine, string output_node_name, int data_size, ref int inference_result);
+
+        [DllImport(openvino_dll_path, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        public extern static void core_delet(IntPtr inference_engine);
+    }
+}
---
+++ PaddleOCR文字识别/PutText.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace OpenVinoSharpPaddleOCR
+{
+    internal class PutText
+    {
+        public static Mat put_text(Mat image, Rect[] rects, string[] texts) {
+            Bitmap bitmap_image = BitmapConverter.ToBitmap(image);
+            Graphics g = Graphics.FromImage(bitmap_image);
+            for (int s = 0; s < texts.Length; s++)
+            {
+                String str = texts[s];
+                Font font = new Font("微软雅黑", 10);
+                SolidBrush sbrush = new SolidBrush(System.Drawing.Color.Black);
+                g.DrawString(str, font, sbrush, new System.Drawing.Point(rects[s].X, rects[s].Y));
+            }
+
+            image = BitmapConverter.ToMat(bitmap_image);
+
+            return image;
+
+        }
+    }
+}
