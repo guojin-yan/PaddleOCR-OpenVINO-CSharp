9f52949bae68d63614e930828d6766c15a0dfc43
国进 颜 <guojin_yjs@cumt.edu.cn>
2022-09-21 13:18:52 +0800

    修改

--- PaddleOCR文字识别/Program.cs
+++ PaddleOCR文字识别/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using OpenCvSharp;
+
+namespace OpenVinoSharpPaddleOCR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { }
+    }
+}
