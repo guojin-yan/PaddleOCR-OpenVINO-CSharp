193226c33cae08b3b06d895c556d4a56d728f80c
yanguojin <guojin_yjs@cumt.edu.cn>
2023-12-20 12:20:51 +0800

    Add OCRDetModels class.

---
+++ src/paddleocr/model/detection_models.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaddleOCR
+{
+    internal class OCRDetModels
+    {
+        //https://paddleocr.bj.bcebos.com/PP-OCRv4/chinese/ch_PP-OCRv4_det_infer.tar
+
+        //https://paddleocr.bj.bcebos.com/PP-OCRv4/chinese/ch_PP-OCRv4_det_server_infer.tar
+    }
+}
